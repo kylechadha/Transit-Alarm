31b4ff026dfd2f75c5c14fbf9bd6b1b53c6aec1b
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-06 15:24:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TransitAlarm::Application.config.secret_key_base = '171e177ca94a0529dedc247ec39684b9d842a27588efd0bf01526ae63f04b551f0e62dfc2e614b7878548ef2e5f04373a8810168d23cee9c78e731b8547405cb'
