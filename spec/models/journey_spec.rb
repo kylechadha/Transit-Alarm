767d126f69de4fc1ff6750ee9fc410ef6919c4c0
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-07 00:54:19 -0800

    Add pending tests for Journey model spec

---
+++ spec/models/journey_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Journey do
+
+  before :each do
+    @valid_attributes = {
+      # Still TBD what attributes will be validated
+    }
+  end
+
+  describe 'validations' do
+    xit 'will test our Journey validations'
+  end
+
+  describe 'relationships' do
+    xit 'will test our Journey relationships'
+  end
+
+end
