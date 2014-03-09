0a70666b78d1e2a49ba41786d9cc0f236eaa40ba
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-08 17:38:17 -0800

    Merge Kate's revisions: Show page & Controller tests

--- app/views/journeys/show.json.jbuilder
+++ app/views/journeys/show.json.jbuilder
@@ -1,3 +1,3 @@
 json.journey do |json|
-  json.extract! @journey, :end_lat, :end_lon, :direction, :alert_distance, :alert_type, :id
+  json.extract! @journey, :start_lat, :start_lon, :end_lat, :end_lon, :direction, :alert_distance, :alert_type, :id
 end
--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -3,9 +3,30 @@ require 'spec_helper'
 describe RoutesController do
 
   context 'when format is JSON' do
-    it 'index - returns all routes' do
-      
+    render_views
+    it "index gives back all routes" do
+      Route.create(gtfs_route_id: "1944",
+      short_name: "1",
+      long_name: "Santa Monica Blvd - Ucla",
+      route_type: 3)
+      get 'index', format: 'json'
+
+      routes = JSON.parse(response.body)
+      expect(routes.count).to eq(1)
+    end
+
+  it 'renders the correct fields for a route object in the JSON' do
+      @route = Route.create(gtfs_route_id: "1944",
+      short_name: "1",
+      long_name: "Santa Monica Blvd - Ucla",
+      route_type: 3)
+      get 'index', format: 'json'
+
+      routes = JSON.parse(response.body)['routes'][0]
+      expect(routes['gtfs_route_id']).to eq @route.gtfs_route_id
+      expect(routes['short_name']).to eq @route.short_name
+      expect(routes['long_name']).to eq @route.long_name
+      expect(routes['route_type']).to eq @route.route_type
     end
   end
-
 end
